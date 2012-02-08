e328f5c932066f34847be17f39e7eeea2ee40be6
Will Mason <will@twilio.com>
2012-02-07 18:08:48 -0800

    Added gitignore, removed cached items, changed DB access, moved mod_rewrite rules to conf.d

--- dbdApp/views/.templates_c/%%A9^A91^A91A76D1%%twiml.open.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.26a, created on 2011-12-14 18:09:03
-         compiled from twiml.open.tpl */ ?>
-<?php echo '<?xml'; ?>
- version="1.0" encoding="UTF-8" <?php echo '?>'; ?>
-
-<Response>
-	<Say voice="woman" language="en-gb">Thank you. Come up to the third floor.</Say>
-	<Play digits="6" />
-</Response>
